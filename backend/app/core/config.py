9e7bc080977578e8afb16a13e7deef2b800c9e3a
Linette Kuhn <linette.kuhn@gmail.com>
2026-04-09 15:36:21 -0400

    Add config file to read environment variables and add virtual environment to gitignore

---
+++ backend/app/core/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+JWT_SECRET: str = os.environ.get("JWT_SECRET", "fallback")
+JWT_ALGORITHM: str = "HS256"
+
+ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+REFRESH_TOKEN_EXPIRE_MINUTE: int = 7 * 24 * 60
